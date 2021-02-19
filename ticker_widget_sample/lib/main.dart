b135c808f2845b15aa8f9e2aa47ec2ad04706d90
Arpit Mathur <mathur.arpit@gmail.com>
2021-02-18 23:22:50 -0500

    updated main file

--- ticker_widget_sample/lib/main.dart
+++ ticker_widget_sample/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticker_widget/ticker_widget.dart';
 import 'package:ticker_widget/tiles/sine_wave_tile.dart';
+import 'package:ticker_widget/tiles/text_tile.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,21 +11,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var tile = SineWaveTile(strokeColor: Colors.green, amplitude: 20);
+    var textTile = TextTile(context:context, text: "Hello World");
+    var sineTile = SineWaveTile(strokeColor: Colors.red, amplitude: 16, period: 60);
 
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.blue),
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-            child: SizedBox(
-                width: 200, height:200,
-                child: Container(
-                    color:Colors.white,
-                    child:TickerWidget(tile, tileAnimDuration: 1,animDirection: -1,)
-                )
+        appBar: AppBar(backgroundColor: Colors.blueAccent, title: Text("Ticker Example"),),
+        body: Column(
+          children: [
+            Expanded(child: SizedBox()),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: SizedBox(
+                height: 50,
+                child: TickerWidget(textTile, tileAnimDuration: 1,animDirection: -1,),
+              ),
+            ),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: SizedBox(
+                height: 50,
+                child: TickerWidget(sineTile, tileAnimDuration: 1,animDirection: -1,),
+              ),
             )
-        ),
+          ],
+        )
       ),
     );
   }
